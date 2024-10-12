44071c24b87d13b4d180f937ebfd85030f303046
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-12 15:54:45 +0200

    Added new variables in scrpt.js to thinker with
    Explored the comperason operators

--- scrpt.js
+++ scrpt.js
@@ -1,2 +1,37 @@
-let name = "Socrated";
+let ini_name = "Socrates";
 let surname = "Bazque";
+let age = 60;
+let age_died = 90;
+const pi = 3.14;
+
+console.log(ini_name);
+console.log(surname);
+console.log(age);
+console.log(age_died);
+console.log(pi);
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23+97);
+
+console.log(2+3+5+7+11+13);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+
+//space here for new thinkering
+
+const myInt = 5;
+const myFloat = 6.667;
+myInt;
+myFloat;
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
