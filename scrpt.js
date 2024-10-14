78270fa3c84fb5eb120dc1c7a2a3ad9360abc609
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-14 16:31:36 +0200

    Continue exercises of TOP string methods
    Added some string methods to console log

--- scrpt.js
+++ scrpt.js
@@ -35,3 +35,56 @@ const lotsOfDecimal = 1.766584958675746364;
 lotsOfDecimal;
 const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
 twoDecimalPlaces;
+
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const badString = string;
+console.log(badString);
+
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+// const greeting = "Hello";
+// const name = "Chris";
+// console.log(greeting + ", " + name); // "Hello, Chris"
+
+const song = "Fight the Youth";
+const score = 6;
+const highestScore = 10;
+const output = `I like the song ${song}. I gave it a score of ${(score / highestScore) * 100}%.`;
+console.log(output); // "I like the song Fight the Youth. I gave it a score of 90%."
+
+const newline = `One day you finally knew
+what you had to do, and began,`;
+console.log(newline);
+
+const goodQuotes1 = 'She said "I think so!"';
+const goodQuotes2 = `She said "I'm not going in there!"`;
+
+const bigmouth = 'I\'ve got no right to take my place…';
+console.log(bigmouth);
+
+const my_name = "Front ";
+const number = 242;
+console.log(my_name + number); // "Front 242"
+
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
+// number
+
+const myNum2 = 123;
+const myString2 = String(myNum2);
+console.log(typeof myString2);
+// string
+
+let text = "Please visit Microsoft and Microsoft!";
+let newText = text.replace(/Microsoft/g, "W3Schools");
+console.log(newText);
